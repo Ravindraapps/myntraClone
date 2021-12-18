059bce6bd5f2008418b0ef60d929e371745b3a77
Suraj Kisanrao Ughade <88610665+Suraj8007@users.noreply.github.com>
2021-12-18 08:38:45 +0530

    my update

--- Project/componants/studio.js
+++
@@ -1,8 +0,0 @@
-const studio = () => {
-    return`<div class="firstCol">
-    <img id="studioImhg" src="https://constant.myntassets.com/web/assets/img/sudio-nav-banner.png" alt="">
-    <button id="exploreStudio">EXPLORE STUDIO  ></button>
-</div>`
-}
-
-export default studio;
